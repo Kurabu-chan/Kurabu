d9eb52590a3d1c6abf77efea20a69961e9c6c647
Rafael Bieze <ikbenrafaelbieze2001@gmail.com>
2020-10-11 15:37:42 +0200

    Fix Home.status error

--- iMAL/src/routes/MainScreens/HomeScreen.tsx
+++ iMAL/src/routes/MainScreens/HomeScreen.tsx
@@ -7,48 +7,52 @@ import { NavigationStackScreenProps } from 'react-navigation-stack';
 import AnimeNodeSource from '../../APIManager/AnimeNodeSource';
 
 type StateType = {
-    key: string,
-    nodeSource: AnimeNodeSource
+    nodes: {
+        key: string,
+        nodeSource: AnimeNodeSource
+    }[]
 }
 
-export default class Home extends React.Component<NavigationStackScreenProps,StateType[]>{
+export default class Home extends React.Component<NavigationStackScreenProps,StateType>{
     constructor(props: NavigationStackScreenProps){
         super(props);
-        this.state = [
-            {
-                key: "Summer 2020:",
-                nodeSource: new SeasonalSource(2020, "summer")
-            },
-            {
-                key: "Summer 2019:",
-                nodeSource: new SeasonalSource(2019, "summer")
-            },
-            {
-                key: "Summer 2018:",
-                nodeSource: new SeasonalSource(2018, "summer")
-            },
-            {
-                key: "Summer 2017:",
-                nodeSource: new SeasonalSource(2017, "summer")
-            }
-            ,
-            {
-                key: "Summer 2016:",
-                nodeSource: new SeasonalSource(2016, "summer")
-            }
-            ,
-            {
-                key: "Summer 2015:",
-                nodeSource: new SeasonalSource(2015, "summer")
-            }
-        ];        
+        this.state = {
+            nodes:[
+                {
+                    key: "Summer 2020:",
+                    nodeSource: new SeasonalSource(2020, "summer")
+                },
+                {
+                    key: "Summer 2019:",
+                    nodeSource: new SeasonalSource(2019, "summer")
+                },
+                {
+                    key: "Summer 2018:",
+                    nodeSource: new SeasonalSource(2018, "summer")
+                },
+                {
+                    key: "Summer 2017:",
+                    nodeSource: new SeasonalSource(2017, "summer")
+                }
+                ,
+                {
+                    key: "Summer 2016:",
+                    nodeSource: new SeasonalSource(2016, "summer")
+                }
+                ,
+                {
+                    key: "Summer 2015:",
+                    nodeSource: new SeasonalSource(2015, "summer")
+                }
+            ]
+        };        
     }
 
     render(){
         return (
             <SafeAreaProvider style={{ backgroundColor: "#1a1a1a" }}>
                 <FlatList
-                    data={this.state}
+                    data={this.state.nodes}
                     renderItem={(item) => (
                         <AnimeList title={item.item.key}
                             animeNodeSource={item.item.nodeSource}
