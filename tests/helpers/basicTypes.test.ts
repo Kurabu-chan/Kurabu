8119beb8eb873b42f43ab5d16badd28b8360fa97
Rafael Bieze <ikbenrafaelbieze2001@gmail.com>
2021-04-10 02:19:03 +0200

    BasicTypes tests

---
+++ tests/helpers/basicTypes.test.ts
@@ -0,0 +1,112 @@
+import { expect } from "chai";
+import {
+	extractFields,
+	Fields,
+	fieldsToString,
+	isErrResp,
+	isTokenResponse,
+} from "../../src/helpers/BasicTypes";
+
+describe("Basic Types", () => {
+	describe("Fields", () => {
+		describe("extractFields", () => {
+			it("extractFields should return empty array for empty string", () => {
+				let input = "";
+
+				let actual = extractFields(input);
+
+				expect(actual).to.be.an("array").that.is.empty;
+			});
+
+			it("extractFields should return fields array for fields list comma-space seperated", () => {
+				let input = "id, title, synopsis, popularity, updated_at, status";
+
+				let actual = extractFields(input);
+
+				expect(actual)
+					.to.contain(Fields.id)
+					.contain(Fields.title)
+					.contain(Fields.synopsis)
+					.contain(Fields.popularity)
+					.contain(Fields.updated_at)
+					.contain(Fields.status);
+			});
+
+			it("extractFields should return fields array for fields list comma seperated", () => {
+				let input = "id,title,synopsis,popularity,updated_at,status";
+
+				let actual = extractFields(input);
+
+				expect(actual)
+					.to.contain(Fields.id)
+					.contain(Fields.title)
+					.contain(Fields.synopsis)
+					.contain(Fields.popularity)
+					.contain(Fields.updated_at)
+					.contain(Fields.status);
+			});
+		});
+
+		describe("fieldsToString", () => {
+			it("fieldsToString should return empty string for empty array", () => {
+				let input: Fields[] = [];
+
+				let actual = fieldsToString(input);
+
+				expect(actual).to.equal("");
+			});
+
+			it("fieldsToString should return array of fields for comma-space seperated list of some fields", () => {
+				let input = [Fields.id, Fields.nsfw, Fields.popularity];
+
+				let actual = fieldsToString(input);
+
+				expect(actual).to.equal("id, nsfw, popularity");
+			});
+		});
+	});
+
+	describe("Responses", () => {
+		describe("isTokenResponse", () => {
+			it("isTokenResponse should return false for non token response", () => {
+				var input = {
+					error: "invalid_token",
+					message: "incorrect token or something",
+				};
+
+				expect(isTokenResponse(input)).to.be.false;
+			});
+
+			it("isTokenResponse should return true for token response", () => {
+				var input = {
+					token_type: "bearer",
+					token: "blabla123",
+					refreshtoken: "blabla123...4",
+				};
+
+				expect(isTokenResponse(input)).to.be.true;
+			});
+		});
+
+		describe("isErrResp", () => {
+			it("isErrResp should return true for error response", () => {
+				var input = {
+					error: "invalid_token",
+					message: "incorrect token or something",
+				};
+
+				expect(isErrResp(input)).to.be.true;
+			});
+
+			it("isErrResp should return false for non error response", () => {
+				var input = {
+					token_type: "bearer",
+					token: "blabla123",
+					refreshtoken: "blabla123...4",
+				};
+
+				expect(isErrResp(input)).to.be.false;
+			});
+		});
+	});
+});
