52822d68e81c39197498df658208d1475ab67de4
Rafael Bieze <ikbenrafaelbieze2001@gmail.com>
2021-07-24 23:39:09 +0200

    Fix rename migration to rename instead of dropping one and making another

--- src/database/migrations/00000002-rename_verif_attempt_count.js
+++ src/database/migrations/00000002-rename_verif_attempt_count.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-var Sequelize = require('sequelize');
+var Sequelize = require("sequelize");
 
 /**
  * Actions summary:
@@ -11,83 +11,59 @@ var Sequelize = require('sequelize');
  **/
 
 var info = {
-    "revision": 2,
-    "name": "rename_verif_attempt_count",
-    "created": "2021-07-24T21:33:41.530Z",
-    "comment": ""
+	revision: 2,
+	name: "rename_verif_attempt_count",
+	created: "2021-07-24T21:33:41.530Z",
+	comment: "",
 };
 
-var migrationCommands = [{
-        fn: "removeColumn",
-        params: ["Users", "VerifAttemptCount"]
-    },
-    {
-        fn: "addColumn",
-        params: [
-            "Users",
-            "verifAttemptCount",
-            {
-                "allowNull": true,
-                "type": Sequelize.INTEGER
-            }
-        ]
-    }
+var migrationCommands = [
+	{
+		fn: "renameColumn",
+		params: ["Users", "VerifAttemptCount", "verifAttemptCount"],
+	},
 ];
 
-var rollbackCommands = [{
-        fn: "removeColumn",
-        params: ["Users", "verifAttemptCount"]
-    },
-    {
-        fn: "addColumn",
-        params: [
-            "Users",
-            "VerifAttemptCount",
-            {
-                "allowNull": true,
-                "type": Sequelize.INTEGER
-            }
-        ]
-    }
+var rollbackCommands = [
+	{
+		fn: "renameColumn",
+		params: ["Users", "verifAttemptCount", "VerifAttemptCount"],
+	},
 ];
 
 module.exports = {
-    pos: 0,
-    up: function(queryInterface, Sequelize)
-    {
-        var index = this.pos;
-        return new Promise(function(resolve, reject) {
-            function next() {
-                if (index < migrationCommands.length)
-                {
-                    let command = migrationCommands[index];
-                    console.log("[#"+index+"] execute: " + command.fn);
-                    index++;
-                    queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
-                }
-                else
-                    resolve();
-            }
-            next();
-        });
-    },
-    down: function(queryInterface, Sequelize)
-    {
-        var index = this.pos;
-        return new Promise(function(resolve, reject) {
-            function next() {
-                if (index < rollbackCommands.length)
-                {
-                    let command = rollbackCommands[index];
-                    console.log("[#"+index+"] execute: " + command.fn);
-                    index++;
-                    queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
-                }
-                else
-                    resolve();
-            }
-            next();
-        });
-    },
-    info: info
+	pos: 0,
+	up: function (queryInterface, Sequelize) {
+		var index = this.pos;
+		return new Promise(function (resolve, reject) {
+			function next() {
+				if (index < migrationCommands.length) {
+					let command = migrationCommands[index];
+					console.log("[#" + index + "] execute: " + command.fn);
+					index++;
+					queryInterface[command.fn]
+						.apply(queryInterface, command.params)
+						.then(next, reject);
+				} else resolve();
+			}
+			next();
+		});
+	},
+	down: function (queryInterface, Sequelize) {
+		var index = this.pos;
+		return new Promise(function (resolve, reject) {
+			function next() {
+				if (index < rollbackCommands.length) {
+					let command = rollbackCommands[index];
+					console.log("[#" + index + "] execute: " + command.fn);
+					index++;
+					queryInterface[command.fn]
+						.apply(queryInterface, command.params)
+						.then(next, reject);
+				} else resolve();
+			}
+			next();
+		});
+	},
+	info: info,
 };
