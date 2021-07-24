2cfb897d0d0f11376410ea4e0b344a36921121b4
Rafael Bieze <ikbenrafaelbieze2001@gmail.com>
2021-07-24 16:42:34 +0200

    apply tslint in createmigration

--- src/database/createmigration.ts
+++ src/database/createmigration.ts
@@ -1,51 +1,56 @@
-import { SequelizeTypescriptMigration } from "sequelize-typescript-migration";
-import { Sequelize, DataType } from "sequelize-typescript";
-import * as path from "path";
-import ModelsArray from "../models";
 import { config } from "dotenv";
+import * as path from "path";
+import {
+	DataType,
+	Sequelize,
+} from "sequelize-typescript";
+import { SequelizeTypescriptMigration } from "sequelize-typescript-migration";
+
+import ModelsArray from "../models";
+
 config();
 
 async function createMigrationTable(sequelize: Sequelize) {
 	const queryInterface = sequelize.getQueryInterface();
 	await queryInterface.createTable("sequelizemeta", {
 		name: {
-			type: DataType.STRING,
 			allowNull: false,
-			unique: true,
 			primaryKey: true,
+			type: DataType.STRING,
+			unique: true,
 		},
 	});
 	await queryInterface.createTable("sequelizemetamigrations", {
-		revision: {
-			type: DataType.INTEGER,
-			allowNull: false,
-			unique: true,
-			primaryKey: true,
-		},
 		name: {
-			type: DataType.STRING,
 			allowNull: false,
+			type: DataType.STRING,
+		},
+		revision: {
+			allowNull: false,
+			primaryKey: true,
+			type: DataType.INTEGER,
+			unique: true,
 		},
 		state: {
-			type: DataType.JSON,
 			allowNull: false,
+			type: DataType.JSON,
 		},
 	});
 }
 
 (async () => {
 	const sequelize = new Sequelize({
-		username: "unsafe",
-		password: "unsafe",
 		database: "imaltest",
+		dialect: "postgres",
 		host: "127.0.0.1",
 		models: ModelsArray,
-		dialect: "postgres",
+		password: "unsafe",
+		username: "unsafe",
 	});
 	createMigrationTable(sequelize);
 	await SequelizeTypescriptMigration.makeMigration(sequelize as any, {
-		outDir: path.join(__dirname, "./migrations"),
 		migrationName: process.argv[2],
+		outDir: path.join(__dirname, "./migrations"),
 		preview: false,
 	});
 })();
