3235aad941b494933e99cb06ac04cb24dbb226af
Rafael Bieze <ikbenrafaelbieze2001@gmail.com>
2021-07-25 16:03:53 +0200

    Delete anime list item

---
+++ src/errors/MAL/MALMediaNotFound.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import MALError from "./MALError";
+
+export default class MALMediaNotFound extends MALError {
+	constructor(message: any) {
+		super(message);
+		this.errorCode = "042";
+		this.httpCode = 404;
+		this.name = "MALMediaNotFound";
+	}
+}
