456d121a72a5600c2d1f878731cf4145d18feac0
Rafael Bieze <ikbenrafaelbieze2001@gmail.com>
2021-08-01 01:58:51 +0200

    Auto apply pending migrations and exit if there is a migration on the db that is not locally present

--- src/helpers/Database.ts
+++ src/helpers/Database.ts
@@ -1,9 +1,13 @@
+import path = require("path");
+import { readdir } from "fs/promises";
 import {
 	Sequelize,
 	SequelizeOptions,
 } from "sequelize-typescript";
 import { singleton } from "tsyringe";
 import { Logger } from "@overnightjs/logger";
+import * as Umzug from "umzug";
+import * as DeepDiff from "deep-diff";
 import ModelsArray, {
 	models,
 	ModelsType,
@@ -47,10 +51,68 @@ export class Database {
 			sequelizeOptions
 		);
 		this._sequelize.databaseVersion().then((version) => {
-			Logger.Info(`Database initialized with db version ${version}`);
+			Logger.Info(`Databases initialized with db version ${version}`);
 		})
 		.catch((err) => {
 			Logger.Err(err)
 		});
+
+		void this.migrate();
+	}
+
+	private async migrate() {
+		let consoleMessages = "";
+		await (async () => {
+			consoleMessages += "Checking migrations\n";
+			const [results, meta] = await this._sequelize.query("SELECT * FROM \"SequelizeMeta\"", {
+				raw: true,
+			});
+
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-assignment
+			const presentMigrations: string[] = (results as any[]).map(x => x.name);
+
+			const migrationsPath = path.join(process.cwd(), "./src/database/migrations");
+			const availableMigrations = await readdir(migrationsPath);
+
+			const diff = DeepDiff.diff(presentMigrations, availableMigrations);
+
+			if (diff !== undefined && diff?.filter(x => x.kind === "D").length > 0) {
+				throw new Error("Found migration on database that was not present on server");
+			}
+
+			for(const present of presentMigrations){
+				consoleMessages += `\t✔ ${present}\n`;
+			}
+
+			if (diff === undefined) {
+				return;
+			}
+
+			for (const notPresent of diff?.filter(x => x.kind === "A")) {
+				// eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-unsafe-member-access
+				consoleMessages += `\t❌ ${(notPresent as any).item.rhs}\n`
+			}
+
+			// eslint-disable-next-line max-len
+			consoleMessages += "Since there were unapplied migrations detected we are now migrating the database to the latest version\n";
+
+			const umzug = new Umzug({
+				migrations: {
+					params: [
+						this._sequelize.getQueryInterface(),
+						this._sequelize
+					],
+					path: migrationsPath,
+				},
+				storage: "sequelize",
+				storageOptions: {
+					sequelize: this._sequelize
+				}
+			});
+
+			void umzug.up();
+		})();
+
+		Logger.Info(consoleMessages);
 	}
 }
