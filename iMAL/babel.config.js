785d6c6cee8170061e3ef3abe77ba8378866a492
Rafael <ikbenrafaelbieze2001@gmail.com>
2021-11-05 00:51:04 +0100

    Update react native

--- iMAL/babel.config.js
+++ iMAL/babel.config.js
@@ -1,4 +1,4 @@
-module.exports = function (api) {
+module.exports = function(api) {
     api.cache(true);
     return {
         presets: ["babel-preset-expo"],
@@ -15,6 +15,7 @@ module.exports = function (api) {
                     },
                 },
             ],
+            'react-native-reanimated/plugin'
         ],
     };
 };
