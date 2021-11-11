11002b43069fab24dc4f402d0cecadb867a6d823
Rafael <ikbenrafaelbieze2001@gmail.com>
2021-11-12 00:50:47 +0100

    Fix linter to also check tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
     ],
     parser: "@typescript-eslint/parser",
     parserOptions: {
-        project: "tsconfig.json",
+        project: ["./tsconfig.json", "./tests/tsconfig.linter.json"],
         sourceType: "module",
     },
     plugins: [
