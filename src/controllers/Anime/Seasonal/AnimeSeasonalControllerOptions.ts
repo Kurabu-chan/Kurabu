feb9b0540fa664685bcdb0675ddafc38ee1f7fbb
Rafael <ikbenrafaelbieze2001@gmail.com>
2021-11-17 00:32:12 +0100

    Remove unused param from anime search controller

--- src/controllers/Anime/Seasonal/AnimeSeasonalControllerOptions.ts
+++ src/controllers/Anime/Seasonal/AnimeSeasonalControllerOptions.ts
@@ -5,7 +5,6 @@ export const controllerName = "seasonal";
 export type Params = {
 	state: string;
 	user: User;
-	animeid?: number;
 	year?: number;
 	season?: "winter" | "spring" | "summer" | "fall";
 	sort?: "anime_score" | "anime_num_list_users";
