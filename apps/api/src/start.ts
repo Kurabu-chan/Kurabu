56fe8eda1b8844783774fb1380096ddbd437ccdf
Rafael <ikbenrafaelbieze2001@gmail.com>
2022-01-16 17:06:36 +0100

    [test] Test workflow

--- apps/api/src/start.ts
+++ apps/api/src/start.ts
@@ -18,7 +18,7 @@ if (PORT === 15000) {
 
 ContainerManager.getInstance();
 
-console.log("bananass");
+console.log("bananasss");
 
 const exampleServer = new ExampleServer();
 exampleServer.start(PORT);
