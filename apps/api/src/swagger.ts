6cd502847ba11299043857c3e72389796f98f0b8
Rafael <ikbenrafaelbieze2001@gmail.com>
2022-01-26 23:43:20 +0100

    [fix] Fix swagger

--- apps/api/src/swagger.ts
+++ apps/api/src/swagger.ts
@@ -4,7 +4,7 @@ import { serve, setup } from "swagger-ui-express";
 export function apply(app: express.Express): void {
     app.use("/api-docs", serve, setup(undefined, {
         swaggerOptions: {
-            url: "/swagger/swagger.json"
+            url: "../swagger/swagger.json"
         }
     }));
     app.use("/swagger", express.static("swagger"));
