6f2ccecb4239b054d68ae73ccd914420889f3bfa
Rafael <ikbenrafaelbieze2001@gmail.com>
2022-04-13 23:20:58 +0200

    [feat] Add discord button

--- apps/app/src/components/CustomDrawerNavigator.tsx
+++ apps/app/src/components/CustomDrawerNavigator.tsx
@@ -1,20 +1,39 @@
 import Authentication from '#api/Authenticate';
 import { Colors } from '#config/Colors';
 import { DrawerItem, DrawerItemList, DrawerContentScrollView, DrawerContentComponentProps } from '@react-navigation/drawer';
-import { SafeAreaView, View, StyleSheet, Dimensions, Alert } from 'react-native';
+import { SafeAreaView, View, StyleSheet, Dimensions, Alert, Linking } from 'react-native';
 import * as Updates from "expo-updates";
 
 export function CustomDrawerContentComponent(props: DrawerContentComponentProps) {
     return (
         <DrawerContentScrollView>
             <SafeAreaView style={styles.container}>
-                <View style={styles.container}>
+                <View
+                    // style={styles.container}
+                >
                     <DrawerItemList descriptors={props.descriptors} navigation={props.navigation} state={props.state} />
 
                 </View>
-            </SafeAreaView>
-            <SafeAreaView>
-                <View>
+                <View style={styles.bottomListContainer}> 
+                    <DrawerItem
+                        label={"Discord"}
+                        onPress={() => {
+                            Alert.alert("Discord?", "Are you sure you want to go to discord?", [
+                                { text: "Cancel", onPress: () => { return; }, style: "cancel" },
+                                {
+                                    text: "Proceed", onPress: () => {
+                                        void Linking.openURL("https://discord.gg/dSvnuSE7Jg");
+                                    }
+                                }
+                            ])
+
+                        }}
+                        activeBackgroundColor={Colors.KURABUPINK}
+                        inactiveBackgroundColor={Colors.ALTERNATE_CONTENT_BACKGROUND}
+                        activeTintColor={"white"}
+                        inactiveTintColor={"white"}
+                        style={styles.drawerItemStyle}
+                    />
                     <DrawerItem
                         label={"Logout"}
                         onPress={() => {
@@ -37,7 +56,6 @@ export function CustomDrawerContentComponent(props: DrawerContentComponentProps)
                     />
                 </View>
             </SafeAreaView>
-
         </DrawerContentScrollView >
     );
 
@@ -46,12 +64,17 @@ export function CustomDrawerContentComponent(props: DrawerContentComponentProps)
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        minHeight: Dimensions.get("window").height - 100
+        minHeight: Dimensions.get("window").height,
     },
     drawerItemStyle: {
         width: "100%",
         margin: 0,
-        borderRadius: 2,
+        borderRadius: 2
+    },
+    bottomListContainer: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        marginBottom: 60,
     }
     // bottomPart: {
     //     flexDirection: 'row',
