96a64da87282c7e521bc5c9a608fdc7d3c4d694d
Rafael <ikbenrafaelbieze2001@gmail.com>
2022-05-10 00:01:58 +0200

    [feat] Better repository implementation with better type completion

--- apps/auth-api/src/providers/Repository.ts
+++ apps/auth-api/src/providers/Repository.ts
@@ -1,22 +1,15 @@
 import { Inject, Injectable } from "@tsed/di";
 import { IEntity } from "src/entities/IEntity";
-import { knex, Knex } from "knex";
 import { DatabaseService } from "./DatabaseService";
 
 type EntityConstructor<TEntity extends IEntity> = new () => TEntity
 
 @Injectable()
 export class Repository<TEntity extends IEntity> {
-    private repo?: Knex<TEntity, unknown[]>;
-
     constructor(@Inject(DatabaseService) private readonly databaseService: DatabaseService) {
     }
 
     public getRepository(entity: EntityConstructor<TEntity>) {
-        if (!this.repo) {
-            this.repo = knex<TEntity>(entity.name);
-        }
-
-        return this.repo;
+        return this.databaseService.getDatabase()<TEntity>(entity.name);
     }
 }
