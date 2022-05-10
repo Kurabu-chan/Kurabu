74d3d3b44066e9394b71ebd0bb637d5ac7a7c884
Rafael <ikbenrafaelbieze2001@gmail.com>
2022-05-10 23:43:31 +0200

    [feat] Create RolesController in manage folder

---
+++ apps/auth-api/src/controllers/api/manage/roles/RolesController.ts
@@ -0,0 +1,24 @@
+import { Controller, Inject } from "@tsed/di";
+import { InternalServerError } from "@tsed/exceptions";
+import { Get } from "@tsed/schema";
+import { GetUsersQueryHandler } from "../../../../queries/Users/getUsersQuery";
+
+@Controller("/roles")
+export class RolesController {
+    constructor(
+        @Inject(GetUsersQueryHandler) private readonly getUserQueryHandler: GetUsersQueryHandler
+    ) {
+
+    }
+
+    @Get("/")
+    async get() {
+        const result = await this.getUserQueryHandler.handle();
+
+        if (!result.success) {
+            return new InternalServerError("Couldn't get users");
+        }
+
+        return result.data;
+    }
+}
