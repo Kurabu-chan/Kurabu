1d37931cb6781d5c92fe97ced8a890c210255ef4
Rafael <ikbenrafaelbieze2001@gmail.com>
2022-05-17 22:31:42 +0200

    [feat] Add certificate provider

--- apps/auth-api/src/index.ts
+++ apps/auth-api/src/index.ts
@@ -3,12 +3,14 @@ import { PlatformExpress } from "@tsed/platform-express";
 import { InjectorService } from "@tsed/di";
 import { Server } from "./Server";
 import { DatabaseService } from "./providers/DatabaseService";
+import { CertificateProvider } from "./providers/CertificateProvider";
 
 async function bootstrap() {
     try {
         const platform = await PlatformExpress.bootstrap(Server);
         const injector = platform.injector;
         const database = await initializeDatabase(injector);
+        injector.get<CertificateProvider>(CertificateProvider); // load certificates
 
 		await platform.listen();
 
