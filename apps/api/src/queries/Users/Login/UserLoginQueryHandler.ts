3917a38fb86ead76b0a75dbf9d031b4c07656f3b
Rafael <ikbenrafaelbieze2001@gmail.com>
2022-05-28 23:58:43 +0200

    [feat] Use common project's hashing in api

--- apps/api/src/queries/Users/Login/UserLoginQueryHandler.ts
+++ apps/api/src/queries/Users/Login/UserLoginQueryHandler.ts
@@ -1,4 +1,5 @@
 import { autoInjectable } from "tsyringe";
+import { HashingProvider, KeyProvider } from "@kurabu/common";
 import { UserStatusQueryHandler } from "../Status/UserStatusQueryHandler";
 import { UserLoginQuery } from "./UserLoginQuery";
 import { UserLoginQueryResult } from "./UserLoginQueryResult";
@@ -6,12 +7,16 @@ import { IQueryHandler, IQueryResultStatus } from "#queries/IQuery";
 import BadLoginError from "#errors/Authentication/BadLoginError";
 import TokensNotPresentError from "#errors/Authentication/TokensNotPresentError";
 import { Database } from "#helpers/Database";
-import * as hasher from "#helpers/Hasher";
 import { Tokens } from "#models/Tokens";
 
 @autoInjectable()
 export class UserLoginQueryHandler implements IQueryHandler<UserLoginQuery, UserLoginQueryResult> {
-    constructor(private database: Database, private _userStatus: UserStatusQueryHandler) {}
+    constructor(
+        private database: Database,
+        private _userStatus: UserStatusQueryHandler,
+        private _hashingProvider: HashingProvider,
+        private _keyProvider: KeyProvider
+    ) {}
 
     async handle(query: UserLoginQuery): Promise<UserLoginQueryResult> {
         const user = await this.database.models.user.findOne({
@@ -23,7 +28,9 @@ export class UserLoginQueryHandler implements IQueryHandler<UserLoginQuery, User
 
         if (!user) throw new BadLoginError("Incorrect login");
 
-        if ((await hasher.verify(query.password, user.pass)) === false)
+        const encryptionkey = this._keyProvider.getKey("encr");
+
+        if (this._hashingProvider.verify(query.password, user.pass, encryptionkey) === false)
             throw new BadLoginError("Incorrect login");
 
         if (!user.tokensId) throw new TokensNotPresentError("No tokens present on database");
