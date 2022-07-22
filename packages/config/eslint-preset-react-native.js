d4b2d280c6050dc6dfb55aa0ed3ba344d11d33c4
Rafael <ikbenrafaelbieze2001@gmail.com>
2022-07-22 02:44:57 +0200

    [fix] Set no-unused-styles to 0, since it will be difficult for eslint to recognise used and unused styles

--- packages/config/eslint-preset-react-native.js
+++ packages/config/eslint-preset-react-native.js
@@ -13,7 +13,7 @@ const parserOptions = {
         "react-native"
     ],
     rules: {
-        "react-native/no-unused-styles": 2,
+        "react-native/no-unused-styles": 0,
         "react-native/split-platform-components": 2,
         "react-native/no-inline-styles": 2,
         "react-native/no-color-literals": 2,
