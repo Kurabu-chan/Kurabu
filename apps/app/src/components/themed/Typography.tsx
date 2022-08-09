f11e247c25b403ad88cdf5bd18a9405bc357f7c0
Rafael <ikbenrafaelbieze2001@gmail.com>
2022-08-09 16:53:56 +0200

    [feat] Add createTypographyStyles function to typography

--- apps/app/src/components/themed/Typography.tsx
+++ apps/app/src/components/themed/Typography.tsx
@@ -1,81 +1,114 @@
 import { ThemedStyleSheet } from "#helpers/ThemedStyleSheet";
-import { AppliedStyles, colors, FontCasing, FontStyle, FontWeight, MainColorSets, ProvidedTheme, resolve, ThemedComponent, TypographicColorSetSetting, typography, TypographyScales } from "@kurabu/theme";
+import {
+	AppliedStyles,
+	colors,
+	FontCasing,
+	FontStyle,
+	FontWeight,
+	MainColorSets,
+	ProvidedTheme,
+	resolve,
+	ThemedComponent,
+	TypographicColorSetSetting,
+	typography,
+	TypographyScales
+} from "@kurabu/theme";
 import React from "react";
-import { TextProps, Text, View } from "react-native";
+import { TextProps, Text, TextStyle, StyleProp } from "react-native";
 
 type Props = {
-    variant: TypographyScales,
-    colorVariant: MainColorSets,
-    isOnContainer: boolean,
-    textKind: TypographicColorSetSetting
+	variant: TypographyScales,
+	colorVariant: MainColorSets,
+	isOnContainer: boolean,
+	textKind: TypographicColorSetSetting
 } & TextProps;
 
 const RobotoFontMap: Record<FontWeight, string> = {
-    "bold": "Roboto_Bold",
-    "normal": "Roboto_Medium",
-    "100": "Roboto_Thin",
-    "200": "Roboto_Light",
-    "300": "Roboto_Light",
-    "400": "Roboto_Regular",
-    "500": "Roboto_Medium",
-    "600": "Roboto_Medium",
-    "700": "Roboto_Bold",
-    "800": "Roboto_Bold",
-    "900": "Roboto_Black",
+	"bold": "Roboto_Bold",
+	"normal": "Roboto_Medium",
+	"100": "Roboto_Thin",
+	"200": "Roboto_Light",
+	"300": "Roboto_Light",
+	"400": "Roboto_Regular",
+	"500": "Roboto_Medium",
+	"600": "Roboto_Medium",
+	"700": "Roboto_Bold",
+	"800": "Roboto_Bold",
+	"900": "Roboto_Black",
 };
 
+export function createTypographyStyles(
+	variant: TypographyScales,
+	textKind: TypographicColorSetSetting,
+	isOnContainer: boolean,
+	colorVariant: MainColorSets): [Styles, (styles: AppliedStyles<Styles>, theme: ProvidedTheme, propertyStyles: StyleProp<TextStyle>) => StyleProp<TextStyle>] {
+	const styles: Styles = ThemedStyleSheet.create({
+		text: {
+			fontSize: typography.fontSize(variant),
+			fontWeight: typography.fontWeight(variant),
+			fontFamily: typography.fontFamily(variant),
+			letterSpacing: typography.letterSpacing(variant),
+			fontStyle: typography.fontStyle(variant),
+			color: isOnContainer ? colors.onColor(colorVariant, textKind) : colors.onColorContainer(colorVariant, textKind)
+		}
+	})
+
+
+	function apply(styles: AppliedStyles<Styles>, theme: ProvidedTheme, propertyStyles: StyleProp<TextStyle>): StyleProp<TextStyle> {
+
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+		const textCase: FontCasing = resolve(typography.case(variant), theme);
+		const textTransform = textCase === "AllCaps" ? "uppercase" : "none";
+
+		if (styles.text[1].fontFamily === "Roboto") {
+			styles.text[1].fontFamily = RobotoFontMap[styles.text[1].fontWeight ?? 400];
+
+
+			if (styles.text[1].fontStyle === "italic") {
+				styles.text[1].fontStyle = "normal";
+				styles.text[1].fontFamily += "_Italic";
+			}
+		}
+
+		if (!Array.isArray(propertyStyles)) {
+			propertyStyles = [propertyStyles];
+		}
+
+		return [
+			{
+				textTransform: textTransform,
+				...styles.text[1]
+			},
+			...propertyStyles
+		]
+	}
+
+	return [styles, apply];
+}
+
 export class Typography extends ThemedComponent<Styles, Props> {
-    constructor(props: Props) {
-        const styles = ThemedStyleSheet.create({
-            text: {
-                fontSize: typography.fontSize(props.variant),
-                fontWeight: typography.fontWeight(props.variant),
-                fontFamily: typography.fontFamily(props.variant),
-                letterSpacing: typography.letterSpacing(props.variant),
-                fontStyle: typography.fontStyle(props.variant),
-                color: props.isOnContainer ? colors.onColor(props.colorVariant, props.textKind) : colors.onColorContainer(props.colorVariant, props.textKind)
-            }
-        })
+	applyTextStyle: (styles: AppliedStyles<Styles>, theme: ProvidedTheme, propertyStyles: StyleProp<TextStyle>) => StyleProp<TextStyle>;
 
-        super(styles, props);
-    }
+	constructor(props: Props) {
+		const styles = createTypographyStyles(props.variant, props.textKind, props.isOnContainer, props.colorVariant);
+		super(styles[0], props);
+		this.applyTextStyle = styles[1];
+	}
 
-    renderThemed(styles: AppliedStyles<Styles>, theme: ProvidedTheme): JSX.Element {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        const textCase: FontCasing = resolve(typography.case(this.props.variant), theme);
-        const textTransform = textCase === "AllCaps" ? "uppercase" : "none";
-
-        if (styles.text[1].fontFamily === "Roboto") { 
-            styles.text[1].fontFamily = RobotoFontMap[styles.text[1].fontWeight ?? 400];
-            
-
-            if(styles.text[1].fontStyle === "italic") {
-                styles.text[1].fontStyle = "normal";
-                styles.text[1].fontFamily += "_Italic";
-            }
-        }
-
-        return (
-            <Text {...this.props} style={
-                [
-                    {
-                        textTransform: textTransform,
-                        ...styles.text[1]
-                    },
-                    this.props.style
-                ]
-            }>{this.props.children}</Text>
-        );
-    }
+	renderThemed(styles: AppliedStyles<Styles>, theme: ProvidedTheme): JSX.Element {
+		return (
+			<Text {...this.props} style={this.applyTextStyle(styles, theme, this.props.style)}>{this.props.children}</Text>
+		);
+	}
 }
 
 type Styles = {
-    text: {
-        fontSize: number
-        fontWeight: FontWeight
-        fontFamily: string
-        letterSpacing: number
-        fontStyle: FontStyle
-        color: string
-    }
+	text: {
+		fontSize: number
+		fontWeight: FontWeight
+		fontFamily: string
+		letterSpacing: number
+		fontStyle: FontStyle
+		color: string
+	}
 }
