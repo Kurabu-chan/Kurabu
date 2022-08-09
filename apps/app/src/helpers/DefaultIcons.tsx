08fdac2b7938d59e103426c342c06b511080a50e
Rafael <ikbenrafaelbieze2001@gmail.com>
2022-08-10 01:33:49 +0200

    [feat] Add color functionality to default icons

--- apps/app/src/helpers/DefaultIcons.tsx
+++ apps/app/src/helpers/DefaultIcons.tsx
@@ -1,46 +1,49 @@
 import { Colors } from "#config/Colors";
 import React from "react";
-import { StyleSheet, TextStyle, ViewStyle } from "react-native";
+import { ColorValue, StyleSheet, TextStyle, ViewStyle } from "react-native";
 import { Icon } from "react-native-elements";
 
-export function createSearchIcon(size?: number, style?: ViewStyle | TextStyle) {
+export function createSearchIcon(size?: number, style?: ViewStyle | TextStyle, color: number | ColorValue = Colors.TEXT) {
     return (<Icon
         name={"search"}
         tvParallaxProperties={undefined}
-        style={style}
+		style={style}
+		color={color}
         size={size} />
     )
 }
 
-export function createClearIcon(size?: number, style?: ViewStyle | TextStyle) {
+export function createClearIcon(size?: number, style?: ViewStyle | TextStyle, color: number | ColorValue = Colors.TEXT) {
     return (<Icon
         name={"close"}
         tvParallaxProperties={undefined}
         style={style}
+		color={color}
+
         size={size} />
     )
 }
 
-export function createIconAnime(size?: number, style?: ViewStyle | TextStyle) {
+export function createIconAnime(size?: number, style?: ViewStyle | TextStyle, color: number | ColorValue = Colors.TEXT) {
     return (
         <Icon
             name="film"
             type="font-awesome-5"
-            color={Colors.TEXT}
             size={size}
+			color={color}
             style={{...styles.noPadding, ...style}}
             tvParallaxProperties={undefined}
         />
     );
 }
 
-export function createIconManga(size?: number, style?: ViewStyle | TextStyle) {
+export function createIconManga(size?: number, style?: ViewStyle | TextStyle, color: number | ColorValue = Colors.TEXT) {
     return (
         <Icon
             name="book"
             type="font-awesome-5"
-            color={Colors.TEXT}
             size={size}
+			color={color}
             style={{ ...styles.noPadding, ...style }}
             tvParallaxProperties={undefined}
         />
